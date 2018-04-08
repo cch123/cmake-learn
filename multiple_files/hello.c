8ac2b7feb172b48dbd63320caf99f785e28e7d52
Xargin <cao1988228@163.com>
2018-04-08 21:20:41 +0800

    add mul files example

---
+++ multiple_files/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "p.h"
+
+int main() {
+    print();
+    printf("hello world!\n");
+}
---
+++ multiple_files/p.c
@@ -0,0 +1,6 @@
+#include "p.h"
+#include <stdio.h>
+
+void print() {
+    printf("oh print in p.c file\n");
+}
